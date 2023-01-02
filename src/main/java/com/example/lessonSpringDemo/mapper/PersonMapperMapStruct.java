c221021a84b64a38c2add222c442b44b3c3a63a2
”Aliaksandr <moroz.ay@esphere.ru>
2023-01-02 20:12:03 +0300

    MS

--- src/main/java/com/example/lessonSpringDemo/mapper/PersonMapperMapStruct.java
+++ src/main/java/com/example/lessonSpringDemo/mapper/PersonMapperMapStruct.java
@@ -5,11 +5,18 @@ import com.example.lessonSpringDemo.entity.Person;
 
 import lombok.NoArgsConstructor;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface PersonMapperMapStruct {
+    @Mappings({
+            @Mapping(source = "name", target = "name"),
+            @Mapping(source = "lastname", target = "lastname"),
+            @Mapping(source = "email", target="email")
+    })
     PersonResponseDTO toPersonResponseDTO(Person person);
     List<PersonResponseDTO> toPersonResponseDTOList(List<Person> personList);
 }
